f160deb19e655accf77aee15357b432262c1a6c1
wangyonghui <coder_hui@163.com>
2021-01-24 18:26:38 +0800

    update run.py

--- testcases/run.py
+++ testcases/run.py
@@ -3,14 +3,15 @@ import time
 import pytest
 import subprocess
 
+sys.path.append("../")
+
 from common.config import root_path
 
 sys.path.append(root_path)
+
 current_time = time.strftime("%Y%m%d%H%M", time.localtime(time.time()))
 
-
 if __name__ == "__main__":
-
     # allure 报告生成路径
     report_path = "../report/report_{}".format(current_time)
     report_html_path = report_path + "/html"
